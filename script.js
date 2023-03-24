b23c02368aa66df504307661465c5084d0172cb1
Angelo Antu <94218410+ANGELOANTU7@users.noreply.github.com>
2023-03-24 19:09:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ((username == "angelo" && password == "1234")||(username == "heyron" && password == "4567")||(username == "harshed" && password == "9188")||(username == "sameer" && password == "4567")) {
+      window.location.href = "success.html";
+    } else {
+      document.getElementById("message").innerHTML = "Invalid username or password!";
+    }
+  }
+  
